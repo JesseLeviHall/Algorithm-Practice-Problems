23c8bde653a330ec69b8213ff6c06816e03c3d90
jesse h <jesselevihall@gmail.com>
2022-08-06 08:42:46 -0600

    bigOnote

---
+++ bigO.js
@@ -0,0 +1,12 @@
+/*
+Notes from Algo class for flash cards. 
+
+Big O allows us to talk formally about how the runtime of an algorithm grows as the inputs grow. 
+O(n) means runtime grown proportionally to n. Rule of thumb a loop. 
+O(1) means constant time always same number of calculations no matter what n is
+O(n2) means maybe a nested loop, runtime squared by what input is. 
+
+The logarithm of a number (usually log2 as log) roughly measures the number of times you can divide that number by 2 before you get a value thats less than or equal to one. Ex: log(8) = 3
+Shows up in search algorithms, sorting, recursion sometimes involves logarithmic space complexity (not time).
+
+*/
