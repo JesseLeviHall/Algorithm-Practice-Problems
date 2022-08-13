95ca3db75972a0cdc203bda540184e01e74b715a
jesse h <jesselevihall@gmail.com>
2022-08-13 16:06:41 -0600

    stringify numbers

--- leetcode/reverseString.js
+++ leetcode/reverseString.js
@@ -1,13 +1,24 @@
+var reverseString = function (s) {
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    let endTemp = s[s.length - 1 - i];
+    let begTemp = s[i];
 
-var reverseString = function(s) {
-    for (let i = 0; i < Math.floor(s.length/2); i++) {
-      let endTemp = s[s.length -1  - i];
-      let begTemp = s[i];
-
-      s[s.length - 1- i] = begTemp;
-      s[i] = endTemp
-    }
-    return s;
+    s[s.length - 1 - i] = begTemp;
+    s[i] = endTemp;
+  }
+  return s;
 };
 
-console.log(reverseString(["h","e","l","l","o"]))
+//console.log(reverseString(["h", "e", "l", "l", "o"]));
+
+function reverse(str) {
+  if (str.length <= 1) return str;
+  return reverse(str.slice(1)) + str[0];
+}
+
+function reverseArr(arr) {
+  if (arr.length <= 1) return arr;
+  return reverseArr(arr.slice(1)) + ", " + arr[0];
+}
+
+console.log(reverseArr(["ant", "bison", "camel", "duck", "elephant"]));
