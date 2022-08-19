a6d88b5ec363f51c6336e550a12bd7c689398cc8
jesse h <jesselevihall@gmail.com>
2022-08-19 14:26:30 -0600

    data structures

---
+++ archetypes/data structures/general.js
@@ -0,0 +1,14 @@
+/*
+ES 2015 class syntax is syntactical sugar.
+creating a new object must be called constructor.
+the class keyword creates a constant, so you can not redefine it.
+the syntax:
+class Student {
+  constructor (firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+let firstStudent = new Student("Colt", "Steele");
+let secondStudent = new Student("blue", "Steele");
+*/
