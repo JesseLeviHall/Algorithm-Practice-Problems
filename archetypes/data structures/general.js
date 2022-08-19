bccacd3153dd1582aabf57c3e32eba75fac964e9
jesse h <jesselevihall@gmail.com>
2022-08-19 14:48:15 -0600

    instance methodss

--- archetypes/data structures/general.js
+++ archetypes/data structures/general.js
@@ -8,7 +8,20 @@ class Student {
     this.firstName = firstName;
     this.lastName = lastName;
   }
+  markLate() {
+    this.tardies += 1;
+    return `${this.firstName} ${this.lastName} has been late ${this.tardies} times. `;
+  }
+  static enrollStudents(...students){
+    //maybe send and email here
+    return "enrolling students..."
+  }
 }
+-class defined the pattern, new creates the object.
 let firstStudent = new Student("Colt", "Steele");
 let secondStudent = new Student("blue", "Steele");
+
+Instance Methods: ex: markLate() method that acts on a single instance of the constructed object. 
+
+Class Methods: keyword 'static' ex: enrollStudents, define methods pertinent to classes rather than instance objects of the class.
 */
