00a9479669e77702d9a76c00be11c1cae5519e57
jesse h <jesselevihall@gmail.com>
2022-08-22 17:47:45 -0600

    singly linked start

---
+++ archetypes/data structures/singlylinkedlist.js
@@ -0,0 +1,3 @@
+/*
+a data structure that contains a head tail and length property. Consists of nodes (elements), and each node has a value and a pointer to another node or null. There is no index, so inserting and deleting doesnt require a new index for every element after. Accessing one element is more costly than arrays though. 
+*/
