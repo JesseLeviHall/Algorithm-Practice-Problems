c859d5ba6e40480340cc84433f9494a61c4f2f29
jesse h <jesselevihall@gmail.com>
2022-08-22 22:55:12 -0600

    class node

--- archetypes/data structures/singlylinkedlist.js
+++ archetypes/data structures/singlylinkedlist.js
@@ -1,3 +1,26 @@
 /*
 a data structure that contains a head tail and length property. Consists of nodes (elements), and each node has a value and a pointer to another node or null. There is no index, so inserting and deleting doesnt require a new index for every element after. Accessing one element is more costly than arrays though. 
+
+A singly linked list stores a node of data - val
+and a reference to the next node - next
 */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {}
+}
+
+var list = new SinglyLinkedList();
+list.push("Hello");
+list.push("goodbye");
