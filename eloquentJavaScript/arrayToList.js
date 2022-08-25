63d3da1a348d31d715d2dbdd1fce2e80a2d0b54d
jesse h <jesselevihall@gmail.com>
2022-08-24 18:09:57 -0600

    array to list

--- eloquentJavaScript/arrayToList.js
+++ eloquentJavaScript/arrayToList.js
@@ -1,33 +1,31 @@
 function arrayToList(arr) {
-
- let list = {};
- let workingList = list;
- for (let i = 0; i < arr.length-1; i++ ) {
-  workingList.value = arr[i];
-  workingList.rest = {};
-  workingList = workingList.rest;
- }
- workingList.value = arr[arr.length - 1];
- workingList.rest = null;
- return list;
+  let list = {};
+  let workingList = list;
+  for (let i = 0; i < arr.length - 1; i++) {
+    workingList.value = arr[i];
+    workingList.rest = {};
+    workingList = workingList.rest;
+  }
+  workingList.value = arr[arr.length - 1];
+  workingList.rest = null;
+  return list;
 }
 
-let list = arrayToList([1,2,3])
+let list = arrayToList([1, 2, 3]);
 
-// helper function 
-
+// helper function
 function prepend(elem, currList) {
- let newList = {
-  value: elem,
-  rest: currList
- }
- return newList
+  let newList = {
+    value: elem,
+    rest: currList,
+  };
+  return newList;
 }
 
 function nth(list, idx) {
- if (idx === 0) return list;
- else return nth(list.rest, idx - 1);
+  if (idx === 0) return list;
+  else return nth(list.rest, idx - 1);
 }
 
 let result = prepend(7, list);
-console.log(nth(result, 2))
+console.log(nth(result, 2));
--- other/other.js
+++ other/other.js
@@ -2,19 +2,16 @@
 // count number of 1s
 
 function countSubstrings(s) {
-   
- let ones = 0;
+  let ones = 0;
 
- for (let i = 0; i < s.length; i++) {
-    
-    let char = s[i]
-    if (char == '1') {
-     ones++
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (char == "1") {
+      ones++;
     }
-   }
-
-   return ones * Math.floor((ones - 1)/2)
+  }
+  return ones * Math.floor((ones - 1) / 2);
 }
 
-console.log(countSubstrings('1000101'))
-console.log(countSubstrings('10010101'))
+console.log(countSubstrings("1000101"));
+console.log(countSubstrings("10010101"));
