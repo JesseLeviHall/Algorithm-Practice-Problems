5f27c18105edad67789231ab9d71972adb8a2040
jesse h <jesselevihall@gmail.com>
2022-08-24 18:11:31 -0600

    1twosum

--- leetcode/1TwoSum.js
+++ leetcode/1TwoSum.js
@@ -1,18 +1,13 @@
-var twoSum = function(nums, target) {
-    
- let map = new Map();
-  
- for (let i = 0; i < nums.length; i++) {
-     
-     let difference = target - nums[i];
-     
-      if (map.has(difference)) {
-             return [map.get(difference), i]
-         }
-     
-     map.set(nums[i], i);
+var twoSum = function (nums, target) {
+  let map = new Map();
 
- }
+  for (let i = 0; i < nums.length; i++) {
+    let difference = target - nums[i];
 
-  
+    if (map.has(difference)) {
+      return [map.get(difference), i];
+    }
+
+    map.set(nums[i], i);
+  }
 };
