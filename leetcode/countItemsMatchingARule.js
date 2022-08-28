d2b093078a11759c0b8e30a4bb1e95caa35362a4
jesse h <jesselevihall@gmail.com>
2022-08-27 18:13:08 -0600

    count items

--- leetcode/countItemsMatchingARule.js
+++ leetcode/countItemsMatchingARule.js
@@ -1,17 +1,16 @@
 // https://leetcode.com/problems/count-items-matching-a-rule/
 
-var countMatches = function(items, ruleKey, ruleValue) {
-    
- let index;
- let count = 0;
- 
- if (ruleKey == "type") index = 0;
- if (ruleKey == "color") index = 1;
- if (ruleKey == "name") index = 2;
- 
- items.forEach(item => {
-     if (item[index] === ruleValue) count++
- })
- 
- return count;
+var countMatches = function (items, ruleKey, ruleValue) {
+  let index;
+  let count = 0;
+
+  if (ruleKey == "type") index = 0;
+  if (ruleKey == "color") index = 1;
+  if (ruleKey == "name") index = 2;
+
+  items.forEach((item) => {
+    if (item[index] === ruleValue) count++;
+  });
+
+  return count;
 };
