d5ad75d57fbe066b266ee6fda0a4977f29b54d42
jesse h <jesselevihall@gmail.com>
2022-09-04 18:50:37 -0600

    singly linked push solution

--- archetypes/data structures/singlylinkedlist.js
+++ archetypes/data structures/singlylinkedlist.js
@@ -19,7 +19,18 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
-  push(val) {}
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 var list = new SinglyLinkedList();
