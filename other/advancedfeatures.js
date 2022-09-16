f94f490c2f75b429f9496b1bba2ca6495de65d24
jesse h <jesselevihall@gmail.com>
2022-09-16 13:42:14 -0600

    from object to map

--- other/advancedfeatures.js
+++ other/advancedfeatures.js
@@ -1,10 +1,10 @@
-function greet(
+/* function greet(
   firstName,
   lastName,
   { salutation = "Hello", suffix = "Mr" } = {}
 ) {
   console.log(`${salutation} ${firstName} ${lastName} ${suffix}`);
-}
+} */
 
 //the default value for the options object is an empty object. this is so that if the options object is not passed in, the function will not throw an error.
 
@@ -14,6 +14,60 @@ function greet(
 OPtional chaining is a feature that allows you to read the value of a property located deep within a chain of connected objects without having to check that each reference in the chain is valid. ex. const name = user?.profile?.name;
 */
 
-/* Map is a collection of keyed data items, just like an Object. But the main difference is that Map allows keys of any type. ex. let map = new Map(); map.set('1', 'str1'); map.set(1, 'num1'); map.set(true, 'bool1'); // remember the regular Object? it would convert keys to string map.keys(); // Map Iterator {"1", 1, true} map.values(); // Map Iterator {"str1", "num1", "bool1"} */
+/* Map is a collection of keyed data items, just like an Object. But the main difference is that Map allows keys of any type and the order is how they're entered. Objects dont have an order. ex. let map = new Map(); map.set('1', 'str1'); A good usecase is for a bunch of user settings */
 
 /* Set is a special type collection – “set of values” (without keys), where each value may occur only once. ex. let set = new Set(); let john = { name: "John" }; let pete = { name: "Pete" }; let mary = { name: "Mary" }; // visits, some users come multiple times set.add(john); set.add(pete); set.add(mary); set.add(john); set.add(mary); // set keeps only unique values for(let user of set) { alert(user.name); // John (then Pete and Mary) } */
+
+/* const map = new Map([
+  [{ a: 1 }, "USD"],
+  [{ b: 2 }, "EUR"],
+  [false, "Rupee"],
+]);
+
+map.forEach((value, key) => {
+  console.log(key, value);
+}); */
+
+//methods on maps:
+//console.log(map.get());
+//mao.set();
+//map.delete();
+//map.clear();
+//map.size;
+//map.has();
+/* 
+const items = [
+  {
+    id: 1,
+    name: "Test",
+    description: "Description",
+  },
+  {
+    id: 2,
+    name: "Test",
+    description: "Description2",
+  },
+  {
+    id: 3,
+    name: "Test",
+    description: "Description3",
+  },
+];
+
+function getItems(id) {
+  return items.find((item) => item.id === id);
+}
+
+console.log(getItems(2));
+
+const itemMap = new Map([
+  [1, { id: 1, name: "Test", description: "Description" }],
+  [2, { id: 2, name: "Test", description: "Description2" }],
+  [3, { id: 3, name: "Test", description: "Description3" }],
+]);
+
+function getItemsMap(id) {
+  return itemMap.get(id);
+}
+
+console.log(getItemsMap(2)); */
