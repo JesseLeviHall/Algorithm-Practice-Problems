e140eeabc6c28043d287fbf3500cdd4f4ae00202
jesse h <jesselevihall@gmail.com>
2022-09-15 18:25:06 -0600

    cleanup

--- terminology.js
+++ terminology.js
@@ -85,5 +85,4 @@ General Terminology:
 
   composition vs inheritance is a way to reuse code. composition is when you use objects to compose new functionality. inheritance is when you use classes to inherit functionality from a parent class.
 
-  Object Oriented Programming (OOP) is a programming paradigm based on the concept of "objects", which can contain data, in the form of fields, often known as attributes; and code, in the form of procedures, often known as methods. A feature of objects is that an object's procedures can access and often modify the data fields of the object with which they are associated (objects have a notion of "this" or "self"). In OOP, computer programs are designed by making them out of objects that interact with one another.[1] OOP languages are diverse, but the most popular ones are class-based, meaning that objects are instances of classes, which also determine their types.
 */
