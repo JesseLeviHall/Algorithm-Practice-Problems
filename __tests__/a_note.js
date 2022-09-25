cc52816c3627fbf58ae4ded61185fcfed8c0696f
jesse h <jesselevihall@gmail.com>
2022-09-24 20:28:13 -0600

    test subtract

--- __tests__/a_note.js
+++ __tests__/a_note.js
@@ -9,3 +9,9 @@ test("sum adds numbers", () => {
   const expected = 10;
   expect(result).toBe(expected);
 });
+
+test("subtract subtracts numbers", () => {
+  const result = subtract(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
