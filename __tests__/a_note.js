ec6a92766a31cea8b4ca7a3c77cc474a113cad2c
jesse h <jesselevihall@gmail.com>
2022-09-24 20:27:16 -0600

    sum . . math

--- __tests__/a_note.js
+++ __tests__/a_note.js
@@ -1,3 +1,5 @@
 //mock function api for testing with jest.
 //refer to docs for common matchers.
 //there is a big difference between toBe and toEqual, which is passby reference vs passby value.
+
+const { sum, subtract } = require("../math");
