08e968fc837a5797b53ec9768dd714746afa7c9e
jesse h <jesselevihall@gmail.com>
2022-10-04 17:22:53 -0600

    two pointers

--- leetcode/1TwoSum.js
+++ leetcode/1TwoSum.js
@@ -1,4 +1,4 @@
-var twoSum = function (nums, target) {
+/* var twoSum = function (nums, target) {
   let map = new Map();
 
   for (let i = 0; i < nums.length; i++) {
@@ -11,40 +11,30 @@ var twoSum = function (nums, target) {
     map.set(nums[i], i);
   }
 };
+*/
 
-function twoSum(numbers, target) {
-  // store the output array
+const array1 = [2, 7, 11, 15];
+const target1 = 9;
+
+const twoSum = (nums, target) => {
+  //store the output array
   let output = [];
 
-  // loop over numbers input array
-  for (let i = 0; i < numbers.length; i++) {
-    // store current value
-    const leftNum = numbers[i];
-    // store what the compliment value would be, along with its index
-    // we use lastIndexOf in case of duplicates
+  //loop over numbers input array
+  for (let i = 0; i < nums.length; i++) {
+    //set the current value of i to a variable
+    const leftNum = nums[i];
+    //set the compliment value to a variable and its index
     const rightNum = target - leftNum;
-    const rightNumIndex = numbers.lastIndexOf(rightNum);
-
-    // if a compliment number was found (index > -1) and it appears after leftNum, we have found the answer and return the output array
+    console.log(rightNum);
+    const rightNumIndex = nums.lastIndexOf(rightNum);
+    console.log(rightNumIndex);
+    //if a compliment number was found (index > -1) and it appears after leftNum, we have found the answer and return the output array.
     if (rightNumIndex > i) {
       return (output = [i, rightNumIndex]);
     }
   }
-
   return output;
-}
-
-//return the index of the two numbers that add up to the target.
-array1 = [2, 7, 11, 15];
-target1 = 9;
-
-const twoSum = (array, target) => {
-  do {
-    let leftNumber = array[i];
-    let rightNumber = array[array.length - 1];
-    if (leftNumber + rightNumber === target) {
-      return [leftNumber.indexOf, rightNumber.indexOf];
-    }
-    i++;
-  } while (i < array.length);
 };
+
+console.log(twoSum(array1, target1));
